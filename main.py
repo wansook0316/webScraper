812a8038de9e80ce5764bffb8994bc39d3b8e7f8
wansook <wansook0316@gmail.com>
2020-03-04 19:01:23 +0900

    second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs
 
 last_indeed_page = extract_indeed_pages()
+
 indeed_jobs = extract_indeed_jobs(last_indeed_page)
-print(indeed_jobs)
+# print("Main", indeed_jobs[0])
