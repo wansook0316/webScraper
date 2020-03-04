d797a5da39b30662b7110cd1710be99858531b19
wansook <wansook0316@gmail.com>
2020-03-04 19:39:58 +0900

    indeed 완성

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&l=%EA%B2%BD%EA%B8%B0%EB%8F%84+%EC%88%98%EC%9B%90&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class": "pagination"})
@@ -42,7 +42,7 @@ def extract_job(html):
     }
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -53,5 +53,11 @@ def extract_indeed_jobs(last_page):
             job = extract_job(result)
             jobs.append(job)
 
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
 
